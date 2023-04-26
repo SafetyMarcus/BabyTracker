9bc9945a0368fe25917e3c47e2f481fd0ae45ba5
SafetyMarcus <hooperarma@gmail.com>
2023-04-26 23:51:52 +1000

    Refactor and add fab
    Signed-off-by: SafetyMarcus <hooperarma@gmail.com>

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,7 +1,11 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -26,36 +30,68 @@ fun App(
 
     Scaffold(
         topBar = {
-            if (children.isNotEmpty()) TabRow(
-                selectedTabIndex = selectedTabPosition,
-                modifier = Modifier.fillMaxWidth(),
-                divider = { Divider() },
-            ) {
-                children.forEachIndexed { i, child ->
-                    Tab(
-                        selected = selectedTabPosition == i,
-                        onClick = { selectedTabPosition = i },
-                        text = { Text(child.firstName) }
-                    )
-                }
+            Toolbar(children, selectedTabPosition) {
+                selectedTabPosition = it
             }
-        }
+        },
+        floatingActionButton = {
+            FloatingActionButton({
+            }) {
+                Icon(Icons.Default.Add, null)
+            }
+        },
     ) {
-        if (currentTabEvents.isNotEmpty()) LazyColumn(
-            modifier = Modifier.padding(it).fillMaxSize(),
-            contentPadding = PaddingValues(16.dp),
-        ) {
-            items(
-                count = currentTabEvents.size
-            ) {
-                when (val row = currentTabEvents[it]) {
-                    is Rows.Day -> Text(
-                        text = row.label,
-                        style = MaterialTheme.typography.bodyLarge,
-                    )
-                    is Rows.Event -> Event(row)
-                }
-            }
+        EventsList(currentTabEvents, it)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Toolbar(
+    children: SnapshotStateList<Child>,
+    selectedTabPosition: Int,
+    onTabSelected: (Int) -> Unit,
+) = Column {
+    CenterAlignedTopAppBar(
+        title = { Text("Events") },
+    )
+    if (children.isNotEmpty()) TabRow(
+        selectedTabIndex = selectedTabPosition,
+        modifier = Modifier.fillMaxWidth(),
+        divider = { Divider() },
+    ) {
+        children.forEachIndexed { i, child ->
+            Tab(
+                selected = selectedTabPosition == i,
+                onClick = { onTabSelected(i) },
+                text = { Text(child.firstName) }
+            )
+        }
+    }
+}
+
+@Composable
+private fun EventsList(
+    currentTabEvents: List<Rows>,
+    it: PaddingValues
+) = LazyColumn(
+    modifier = Modifier.padding(it).fillMaxSize(),
+    contentPadding = PaddingValues(
+        top = 16.dp,
+        start = 16.dp,
+        end = 16.dp,
+        bottom = 56.dp,
+    ),
+) {
+    items(
+        count = currentTabEvents.size
+    ) {
+        when (val row = currentTabEvents[it]) {
+            is Rows.Day -> Text(
+                text = row.label,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            is Rows.Event -> Event(row)
         }
     }
 }
