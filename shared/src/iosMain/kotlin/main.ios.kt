4e910ac2f1d4bc3f8201af15f39bf8054174f662
SafetyMarcus <hooperarma@gmail.com>
2023-04-28 21:25:51 +1000

    Using correct offset for iOS
    Signed-off-by: SafetyMarcus <hooperarma@gmail.com>

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,16 +1,17 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.gitlive.firebase.firestore.Timestamp
 import dev.gitlive.firebase.firestore.toMilliseconds
-import platform.Foundation.NSDate
-import platform.Foundation.NSDateFormatter
-import platform.Foundation.NSTimeInterval
+import platform.Foundation.*
 
 actual fun getPlatformName(): String = "iOS"
 
 actual fun Timestamp.format(format: String): String {
     val formatter = NSDateFormatter()
+    formatter.dateStyle = NSDateFormatterMediumStyle
     formatter.dateFormat = format
-    return formatter.stringFromDate(NSDate(this.toMilliseconds()))
+    return formatter.stringFromDate(
+        NSDate(timeIntervalSinceReferenceDate = this.seconds.toDouble())
+    )
 }
 
 fun MainViewController(viewModel: MainViewModel) = ComposeUIViewController { App(viewModel) }
